f365bc542244711f41c64f8441ba9ac9b7b182f7
zsd <zhangshuda1998@163.com>
2022-05-28 15:24:44 +0800

    push commit

--- src/main/java/com/zds/git/GitTest.java
+++ src/main/java/com/zds/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello3");
         System.out.println("master test");
         System.out.println("test");
+        System.out.println("push test");
     }
 }
