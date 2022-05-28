946a61a277b91c400a4247ff8037b7dcfc64d9c1
zsd <zhangshuda1998@163.com>
2022-05-28 15:05:06 +0800

    master commit2

--- src/main/java/com/zds/git/GitTest.java
+++ src/main/java/com/zds/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("master test");
     }
 }
